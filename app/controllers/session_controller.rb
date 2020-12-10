dcab2896839366fb145d6fa73165481e005a3135
Jake <jrdriscoll17@gmail.com>
2020-12-10 17:47:04 -0600

    Added Navbar

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,8 +1,12 @@
 class SessionController < ApplicationController
   skip_before_action :require_authenticated_user, except: [:destroy]
-  def new; end
+  def new
+    redirect_to user_path(current_user) if session[:user_id] 
+  end
 
   def create
+    redirect_to user_path(current_user) if session[:user_id]
+
     user = User.find_by(username: params[:user][:username])
 
     if user&.authenticate(params[:user][:password])
